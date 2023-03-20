f9ae106c90ca106dad721a844b4ae60c4082f6eb
XelHaku <tamez.jm@gmail.com>
2023-03-19 19:26:51 -0600

    get market cap rank

---
+++ getBlockchainTokens.js
@@ -0,0 +1,57 @@
+const { ReadFile, SaveFile } = require("./services/rwFile.js");
+const coinGecko = ReadFile("allCoinGeckoTokens");
+console.log(coinGecko.length);
+const axios = require("axios");
+const main = async () => {
+  // ethereum
+  //polygon-pos
+  // arbitrum - one;
+  // optimistic - ethereum;
+  // avalanche
+  // fantom
+  blockchain = "polygon-pos";
+
+  tokens = [];
+  for (let i = 0; i < coinGecko.length; i++) {
+    if (blockchain in coinGecko[i].platforms) {
+      var obj = {
+        id: coinGecko[i]["id"],
+        address: coinGecko[i]["platforms"][blockchain],
+      };
+      tokens.push(obj);
+      // tokens.push(coinGecko[i]["platforms"][blockchain]);
+    }
+  }
+  console.log(tokens);
+  console.log(tokens.length);
+  await sleep(3000);
+
+  //api.coingecko.com/api/v3/coins/polygon-pos/contract/0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3
+  // const response = await fetch(url);
+  tokensA = [];
+  for (let i = 0; i < 3; i++) {
+    // for (let i = 0; i < tokens.length; i++) {
+    try {
+      const url = `https://api.coingecko.com/api/v3/coins/${blockchain}/contract/${tokens[i].address}`;
+      const response = await axios.get(url);
+      tokens[i].rank = response.data.market_data.market_cap_rank;
+      tokens[i].index = i;
+      console.log(i, tokens[i]);
+      await sleep(10000);
+    } catch (e) {
+      console.log(i, "\nERROR waiting 60 seconds\n");
+      await sleep(60000);
+      // i = i;
+    }
+  }
+
+  SaveFile(tokens, blockchain + "_Tokens");
+  // for (var key in response.data.market_data.market_cap_rank) {
+  //   console.log(key);
+  // }
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+main();
--- services/rwFile.js
+++ services/rwFile.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 function ReadFile(fileName) {
   try {
     // const data = fs.readFileSync(`../data/${fileName}.json`, "utf8");
-    const data = fs.readFileSync(`../data/${fileName}.json`, "utf8");
+    const data = fs.readFileSync(`./data/${fileName}.json`, "utf8");
     const jsonList = JSON.parse(data);
     return jsonList;
   } catch (err) {
@@ -14,7 +14,7 @@ function ReadFile(fileName) {
 function SaveFile(file, fileName) {
   var jsonContent = JSON.stringify(file);
 
-  fs.writeFile(`../data/${fileName}.json`, jsonContent, "utf8", function (err) {
+  fs.writeFile(`./data/${fileName}.json`, jsonContent, "utf8", function (err) {
     if (err) {
       console.log("An error occured while writing JSON Object to File.");
       return console.log(err);
